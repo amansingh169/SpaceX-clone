863afe871a88e7fd726b5d083b439b8304ac6003
Aman Singh <as2211075@gmail.com>
2024-04-26 12:19:04 +0530

    Changed picture
    Changed the first section picture and corrected all the indents (4 to 2)

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-var lastScrollTop = 0
-var firstSection = window.innerHeight
-var header = $('#navbar')
+var lastScrollTop = 0;
+var firstSection = window.innerHeight;
+var header = $("#navbar");
 
 $(window).scroll(function (event) {
-    var currentScroll = $(this).scrollTop()
+  var currentScroll = $(this).scrollTop();
 
-    if(currentScroll < firstSection){
-        header.removeClass('dark')
-    }
+  if (currentScroll < firstSection) {
+    header.removeClass("dark");
+  }
 
-    if (currentScroll > lastScrollTop) {
-        // Scrolling down
-        header.slideUp()
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down
+    header.slideUp();
+  } else {
+    // Scrolling up
+    if (currentScroll > firstSection) {
+      header.slideDown(); // Show header
+      header.addClass("dark");
     } else {
-        // Scrolling up
-        if(currentScroll > firstSection){
-            header.slideDown() // Show header
-            header.addClass('dark')
-        }else{
-            header.slideDown() // Show header
-        }
+      header.slideDown(); // Show header
     }
+  }
 
-    lastScrollTop = currentScroll
-})
+  lastScrollTop = currentScroll;
+});
 
-$('#hamburger').click(function(){
-    $(this).toggleClass('ham-open')
-    $('.ham-menu').toggleClass('menu-open')
+$("#hamburger").click(function () {
+  $(this).toggleClass("ham-open");
+  $(".ham-menu").toggleClass("menu-open");
 
-    $(window).scroll(function (event) {
-        $('#hamburger').removeClass('ham-open')
-        $('.ham-menu').removeClass('menu-open')
-    })
-})
+  $(window).scroll(function (event) {
+    $("#hamburger").removeClass("ham-open");
+    $(".ham-menu").removeClass("menu-open");
+  });
+});
